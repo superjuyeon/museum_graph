062424c21d00fba801768ac2e5447114c4334342
superjuyeon <smilekjy1001@naver.com>
2022-07-22 23:13:55 +0900

    graph_V4_add_chatbot

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import json
 from flask import Flask, render_template, request, url_for, redirect, session
 import logging
 
+
+a_json = open('static/datasets/배재_단어.json', encoding = 'utf-8')
+new_dict = json.load(a_json)
+
 app = Flask(__name__, static_url_path='/static')
 
 
@@ -13,7 +17,14 @@ app.config['SESSION_TYPE'] = 'filesystem'
 def index():
     return render_template('main.html')
 
-	
+@app.route('/get_answer', methods=['GET', 'POST'])
+def get_answer():
+    if request.method == 'POST':
+        question = request.form['question_id']
+
+        answer = new_dict[question]
+        return answer
+
 if __name__ == '__main__':
     # threaded=True 로 넘기면 multiple plot이 가능해짐
     app.run(host='0.0.0.0', port=8889, debug=True, threaded=True)
